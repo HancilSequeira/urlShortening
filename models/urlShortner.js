42a969c0f5bb29f9b299fc3de44a1c3d4702147e
Hancil Sequiera <hancil@techjini.com>
2020-03-16 16:58:02 +0530

    Project setup of url shortening and intial code for saving shortend url

---
+++ models/urlShortner.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const shortId = require('shortid');
+
+const urlShortner = new Schema({
+    actualUrl:{
+        type: String,
+        required: true
+    },
+    shortUrl:{
+        type: String,
+        required: true,
+        default: shortId.generate()
+    },
+    created_at : Date,
+    updated_at: Date
+
+});
+
+
+/**
+ * to save date and time
+ */
+urlShortner.pre('save', function(next) {
+    const currentDate = new Date();
+
+    this.updated_at = currentDate;
+
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+    next();
+});
+
+
+const urlShortnerModel = mongoose.model('urlShortner', urlShortner);
+
+module.exports = urlShortnerModel;
